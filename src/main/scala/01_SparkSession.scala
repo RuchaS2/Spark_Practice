8295454dfa9b180b697c3e8c4607c4ce8261d575
r0s0buo <rucha.shinde@walmart.com>
2021-08-26 20:24:44 +0530

    Spark Context and session

---
+++ src/main/scala/01_SparkSession.scala
@@ -0,0 +1,31 @@
+/*
+Spark Session -
+entry point to underlying Spark functionality in order
+to programmatically create Spark RDD, DataFrame and DataSet.
+
+Spark Session also includes all the APIs available in different contexts –
+Spark Context,SQL Context,Streaming Context,Hive Context.
+
+Multiple sessions -
+Spark gives a straight forward API to create a new session which shares the same spark context.
+*/
+
+import org.apache.spark.sql.SparkSession
+
+object SparkTest {
+  def main(args:Array[String])={
+
+    // SparkSession will be created using SparkSession.builder() builder patterns.
+    val spark = SparkSession.builder()
+      .master("local[1]")
+      .appName("SparkByExamples")
+      .getOrCreate();
+    println(spark.version)
+
+    //Accessing all contexts
+    println(spark.sparkContext)
+    println(spark.sqlContext)
+
+  }
+
+}
---
+++ src/main/scala/02_SparkContext.scala
@@ -0,0 +1,33 @@
+/*
+Scala Context
+Since Spark 1.x, Spark SparkContext is an entry point to Spark and defined in org.apache.spark package
+used to programmatically create Spark RDD, accumulators, and broadcast variables on the cluster.
+You can create only one SparkContext per JVM.
+
+SparkContext are also present in SparkSession
+Spark session internally creates the Spark Context.
+ */
+import org.apache.spark.{SparkConf, SparkContext}
+object SparkContext {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("sparkContext").setMaster("local[1]")
+
+    //Register it as a singleton object -  use this to create Spark RDD
+    val sparkContext = new SparkContext(sparkConf)
+
+    println("First SparkContext:")
+    println("APP Name :" + sparkContext.appName)
+    println("Deploy Mode :" + sparkContext.deployMode)
+    println("Master :" + sparkContext.master)
+
+    sparkContext.stop()
+
+    val conf2 = new SparkConf().setAppName("sparkContext-2").setMaster("local[1]")
+    val sparkContext2 = new SparkContext(conf2)
+
+    println("Second SparkContext:")
+    println("APP Name :"+sparkContext2.appName);
+    println("Deploy Mode :"+sparkContext2.deployMode);
+    println("Master :"+sparkContext2.master);
+  }
+}
